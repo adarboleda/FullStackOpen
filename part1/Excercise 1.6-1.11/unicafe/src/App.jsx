c806518031f7adeb4b160d75b815ca87735c2695
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2025-03-27 10:53:01 -0500

    1.7-1.8

--- part1/Excercise 1.6-1.11/unicafe/src/App.jsx
+++ part1/Excercise 1.6-1.11/unicafe/src/App.jsx
@@ -6,9 +6,36 @@ const Button = ({ onClick, text }) => {
 
 const Statistic = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positive = (good / total) * 100 || 0;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <>
+      <h2>Statistic</h2>
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={total} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive + "%"} />
+        </tbody>
+      </table>
+    </>
   );
 };
 
@@ -22,23 +49,13 @@ const App = () => {
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
 
-  const total = good + neutral + bad;
-  const average = (good - bad) / total || 0;
-  const positive = (good / total) * 100 || 0;
-
   return (
     <div>
       <h1>Give feedback</h1>
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
-      <h2>Statistic</h2>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={total} />
-      <Statistic text="average" value={average} />
-      <Statistic text="positive" value={positive + "%"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
