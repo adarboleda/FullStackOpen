470a3cc4a0fd3cb9faf838f7ac0b805babb71351
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2025-04-01 10:38:35 -0500

    Exercises 2.1-2.5

---
+++ part2/Exercises 2.1-2.5/introduction/src/components/Course.jsx
@@ -0,0 +1,46 @@
+const Course = ({ courses }) => {
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <h3>
+      Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+    </h3>
+  );
+};
+
+export default Course;
