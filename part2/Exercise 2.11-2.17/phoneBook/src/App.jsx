3101883c8625ccb1bffb48065b32f29fddd9f7a7
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2025-04-08 12:41:31 -0500

    refactor repository

--- part2/Exercise 2.11-2.17/phoneBook/src/App.jsx
+++ part2/Exercise 2.11-2.17/phoneBook/src/App.jsx
@@ -97,7 +97,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
       <Filter
         searchPerson={searchPerson}
         handleSearchChange={handleSearchChange}
